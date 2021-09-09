c25ef72e4c220db32065098ecd683c96d58708c3
Emma <w12bearcat@yahoo.fr>
2021-09-09 08:58:15 +0200

    "Page confirmation"

---
+++ js/confirmation.js
@@ -0,0 +1,18 @@
+//récupération de l'id de la commande provenant du LS
+const responseId = localStorage.getItem("responseId");
+
+//Structure html de la page confirmation de commande
+const positionElement = document.querySelector("recapCommande");
+const structureConfirmationCommande = `
+<p>Votre commande n°: ${responseId} a bien été prise en compte `;
+
+//injection HTML
+positionElement.insertAdjacentHTML("beforebegin", structureConfirmationCommande);
+
+//effacer tout le LS sauf le formulaire
+function enleverCleLocalStorage(){
+    localStorage.removeItem();
+};
+enleverCleLocalStorage("responseId");
+enleverCleLocalStorage("products");
+enleverCleLocalStorage("contact");
